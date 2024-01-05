1f6d42a87c79699dbe18c255196bb9e146960605
khadeom <khadeom10@gmail.com>
2024-01-05 17:56:44 +0530

    first commit: add notes app and model

---
+++ notes/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Note(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='notes')
+    shared_with = models.ManyToManyField(User, related_name='shared_notes')
+
